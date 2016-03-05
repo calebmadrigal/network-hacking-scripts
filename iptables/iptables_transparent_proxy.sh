bf1a1caefdc3526a0683d56e255348dd2ea5b2ba
Caleb Madrigal <caleb.madrigal@gmail.com>
2016-03-04 21:17:03 -0500

    iptables stuff

---
+++ iptables/iptables_transparent_proxy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-port 8080
